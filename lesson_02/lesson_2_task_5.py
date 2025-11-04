dcf8c7cde891a59b8a21c255aac1e8d098ecf4e0
OlgaBorodenok <borodyonka@gmail.com>
2025-11-04 18:44:38 +0500

    Решения задач к уроку 2

---
+++ lesson_02/lesson_2_task_5.py
@@ -0,0 +1,17 @@
+#Напишите функцию month_to_season(),
+# которая принимает один аргумент — номер месяца — и возвращает название сезона,
+# к которому относится этот месяц. Например, передаем 2, на выходе получаем «Зима».
+
+def month_to_season(month):
+    if month in [12, 1, 2]:
+        return "Зима"
+    elif month in [3, 4, 5]:
+        return "Весна"
+    elif month in [6, 7, 8]:
+        return "Лето"
+    elif month in [9, 10, 11]:
+        return "Осень"
+    else:
+        return "Неверный номер месяца"
+print(month_to_season(2))
+print(month_to_season (13))
